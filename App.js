d7b0ccc82ba3f47240210ec18c4c14c02528436f
Michaelgeo1004 <113682053+Michaelgeo1004@users.noreply.github.com>
2023-03-27 12:39:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+
+export default function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Login />}/> 
+         <Route path="home" element={<Home />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
